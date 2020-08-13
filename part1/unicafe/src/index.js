b0a30fff155ea8203e6e4ceda5241b73bbc924c6
Avi Patel <avidpatel1@gmail.com>
2020-08-12 21:31:35 -0700

    unicafe completed
    Pushing the compelted working code for unicafe

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,14 +7,41 @@ const Button = ({onClick, text}) => {
   )
 }
 
-const Display = ({butn, text}) => {
+const Statistic = ({text, value}) => {
   return (
-    <p>{text} {butn}</p> 
+    <tbody align="left">
+      <tr>
+        <th>{text}</th>
+        <th>{value}</th>
+      </tr>
+    </tbody>
   )
-  
-   
 }
 
+const Statistics = ({good, bad, neutral}) => {
+  const goodScore = good*1
+  const neutralScore = neutral*0
+  const badScore = bad*-1
+  const total = good+bad+neutral
+  const totalScore = goodScore + neutralScore + badScore
+  const avg = totalScore/total;
+  const perPostitive = (good/total)*100 + " %"
+  if (total === 0) {
+    return (
+      <p>No feedback given</p>
+    )
+  }
+  return (
+    <table>
+      <Statistic text="good" value={good}/>
+      <Statistic text="neutral" value={neutral}/>
+      <Statistic text="bad" value={bad}/>
+      <Statistic text="all" value={total}/>
+      <Statistic text="average" value={avg}/>
+      <Statistic text="positive" value={perPostitive}/>
+    </table>
+  ) 
+}
 
 const App = () => {
   // save clicks of each button to own state
@@ -26,7 +53,6 @@ const App = () => {
   const neutralHandle = () => setNeutral(neutral + 1)
   const badHandle = () => setBad(bad + 1)
   
-
   return (
     <div>
       <h1>give feedback</h1>
@@ -34,9 +60,7 @@ const App = () => {
       <Button onClick={neutralHandle} text="neutral"/>
       <Button onClick={badHandle} text="bad"/>
       <h1>statistics</h1>
-      <Display text="good" butn={good}/>
-      <Display text="neutral" butn={neutral}/>
-      <Display text="bad" butn={bad}/>
+      <Statistics good={good} bad={bad} neutral={neutral}/>
     </div>
   )
 }
