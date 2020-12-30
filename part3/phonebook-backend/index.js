cc9964e5f02ae3b389e00aa90cecd3c810265b1e
Avi Patel <avidpatel1@gmail.com>
2020-12-30 00:52:29 -0800

    Added MongoDB databse

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -19,84 +19,75 @@ morgan.token('body', (request, response) => {
 app.use(express.static('build'))
 
 
-let persons = [
-    { 
-        name: "Arto Hellas", 
-        number: "040-123456",
-        id: 1
-    },
-    { 
-        name: "Ada Lovelace", 
-        number: "39-44-5323523",
-        id: 2
-    },
-    { 
-        name: "Dan Abramov", 
-        number: "12-43-234345",
-        id: 3
-      },
-      { 
-        name: "Mary Poppendieck", 
-        number: "39-23-6423122",
-        id: 4
-      }
-]
+const Person = require('./models/person')
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(person => response.json(person))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(p => p.id === id)
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+    .then(person => response.json(person))
+    .catch((error) => next(error))
 })
 
 app.get('/info', (request, response) => {
-    const numPersons = persons.length
-    response.send(`<p>Phonebook has info for ${numPersons} people</p> <p>${new Date()}</p>`)
+    Person.countDocuments({}, (err, count) => {
+        console.log(count)
+        response.send(`<p>Phonebook has info for ${count} people</p> <p>${new Date()}</p>`)
+    })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(p => p.id !== id)
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
-
-const getRandomId = (min, max) => {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min) + min)
-}
 
 app.post('/api/persons', (request, response) => {
     const person = request.body
-    const sameName = persons.filter(p => p.name === person.name )
+    if (!person) {
+        return response.status(400).json({ error: 'content missing' })
+    }
     if (!person.name) {
         return response.status(400).json({ error: 'name is missing' })
     }
     if (!person.number) {
         return response.status(400).json({error: 'number is missing'})
     }
-    if (sameName.length > 0) {
-        return response.status(400).json({error: 'name must be unique'})
-    }
-
-
-    const newPerson = {
+    const p = new Person({
         name: person.name,
         number: person.number,
-        id: getRandomId(1, 100)
-    }
-
-    persons = persons.concat(newPerson)
-    response.json(newPerson)
+    })
+    p.save().then(p => response.json(p))
 })
 
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+        .then(updatedPerson => response.json(updatedPerson))
+        .catch(error => next(error))
+})
+
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+
+app.use(errorHandler)
 
 
 
---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log("The number of parameters you have provided is not sufficent for this program to run!")
+    process.exit(1)
+}
+
+
+const password = process.argv[2]
+const url = `mongodb+srv://fullstack:${password}@cluster.sia2m.mongodb.net/phonebook-db?retryWrites=true&w=majority`
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model("Person", personSchema)
+
+if (process.argv.length === 3) {
+    console.log(`phonebook:`)
+    Person.find({}).then(result => {
+        result.forEach(person => {
+          console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+} else if (process.argv.length >= 5) {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4]
+    })
+    person.save().then(result => {
+        console.log(`added${process.argv[3]} number ${process.argv[4]} to phonebook`)
+        mongoose.connection.close()
+    })
+} 
